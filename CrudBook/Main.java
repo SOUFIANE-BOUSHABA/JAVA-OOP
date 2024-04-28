9b9eb46a54bf12dda7a5699ddee8b7f07af6db16
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-04-28 19:06:01 +0000

    create  the test

---
+++ CrudBook/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Library library = new Library();
+
+     
+        library.addBook(new Book(1, "Java Programming", "John Doe"));
+        library.addBook(new Book(2, "Python for Beginners", "Jane Smith"));
+        library.addBook(new Book(3, "C# Essentials", "David Brown"));
+
+       
+        System.out.println("All Books:");
+        for (Book book : library.getAllBooks()) {
+            System.out.println("ID: " + book.getId() + ", Title: " + book.getTitle() + ", Author: " + book.getAuthor());
+        }
+
+        
+        library.updateBook(2, "Python Programming", "Jane Smith");
+
+        
+        System.out.println("\nUpdated Book:");
+        Book updatedBook = library.getBookById(2);
+        System.out.println("ID: " + updatedBook.getId() + ", Title: " + updatedBook.getTitle() + ", Author: " + updatedBook.getAuthor());
+
+      
+        library.deleteBook(1);
+
+       
+        System.out.println("\nBooks after deletion:");
+        for (Book book : library.getAllBooks()) {
+            System.out.println("ID: " + book.getId() + ", Title: " + book.getTitle() + ", Author: " + book.getAuthor());
+        }
+    }
+}
