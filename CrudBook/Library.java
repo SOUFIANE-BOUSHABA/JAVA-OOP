4976837a3ef4df2fb3ae58c082608917dd5054cd
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-04-28 18:59:12 +0000

    this commit  is for creating the methode to add an book to the list

---
+++ CrudBook/Library.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void updateBook(int id, String title, String author) {
+        for (Book book : books) {
+            if (book.getId() == id) {
+                book.setTitle(title);
+                book.setAuthor(author);
+                break;
+            }
+        }
+    }
+
+    public void deleteBook(int id) {
+        books.removeIf(book -> book.getId() == id);
+    }
+
+    public Book getBookById(int id) {
+        for (Book book : books) {
+            if (book.getId() == id) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public List<Book> getAllBooks() {
+        return new ArrayList<>(books);
+    }
+}
